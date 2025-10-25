0c7c064eb830b93ae356e8869fd933a5c14c2837
EuriiYurii <202401400@iacademy.edu.ph>
2025-10-25 08:52:50 +0800

    Update main.cpp

--- GameEngine_GDV21/main.cpp
+++ GameEngine_GDV21/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void Kirara() {
+	bool dendro = true;
+
+	while (dendro) {
+		cout << "Kirara besto Waifu" << endl;
+	}
+}
+
 int main() {
 	cout << "Hello World~" << endl;
+	Kirara();
 }
